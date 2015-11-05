bcf7f0c22dde464b178f96da95e724aabb3a0d72
MEgor <eguar@tut.by>
2015-11-05 23:47:27 +0300

    Initial commit

---
+++ Sources/Accord.Imaging/Tools.cs
@@ -0,0 +1,91 @@
+﻿// Accord Imaging Library
+// The Accord.NET Framework
+// http://accord.googlecode.com
+//
+// Copyright © César Souza, 2009-2012
+// cesarsouza at gmail.com
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// Some functions adapted from the original work of Peter Kovesi,
+// shared under a permissive MIT license. Details are given below:
+//
+//   Copyright (c) 1995-2010 Peter Kovesi
+//   Centre for Exploration Targeting
+//   School of Earth and Environment
+//   The University of Western Australia
+//
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files (the "Software"), to deal
+//   in the Software without restriction, including without limitation the rights 
+//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+//   of the Software, and to permit persons to whom the Software is furnished to do
+//   so, subject to the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included in all
+//   copies or substantial portions of the Software.
+//   
+//   The software is provided "as is", without warranty of any kind, express or
+//   implied, including but not limited to the warranties of merchantability, 
+//   fitness for a particular purpose and noninfringement. In no event shall the
+//   authors or copyright holders be liable for any claim, damages or other liability,
+//   whether in an action of contract, tort or otherwise, arising from, out of or in
+//   connection with the software or the use or other dealings in the software.
+//   
+
+namespace Accord.Imaging
+{
+    using System;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using AForge.Imaging;
+
+    /// <summary>
+    ///   Static tool functions for imaging.
+    /// </summary>
+    /// 
+    /// <remarks>
+    ///   <para>
+    ///     References:
+    ///     <list type="bullet">
+    ///       <item><description>
+    ///         P. D. Kovesi. MATLAB and Octave Functions for Computer Vision and Image Processing.
+    ///         School of Computer Science and Software Engineering, The University of Western Australia.
+    ///         Available in: <a href="http://www.csse.uwa.edu.au/~pk/Research/MatlabFns/Match/matchbycorrelation.m">
+    ///         http://www.csse.uwa.edu.au/~pk/Research/MatlabFns/Match/matchbycorrelation.m </a>
+    ///       </description></item>
+    ///   </list></para>
+    /// </remarks>
+    /// 
+    public static class Tools
+    {
+
+        private const double SQRT2 = 1.4142135623730951;
+
+
+
+        /// <summary>
+        ///   Compares two rectangles for equality, considering an acceptance threshold.
+        /// </summary>
+        public static bool IsEqual(this Rectangle objA, Rectangle objB, int threshold)
+        {
+            return (Math.Abs(objA.X - objB.X) < threshold) &&
+                   (Math.Abs(objA.Y - objB.Y) < threshold) &&
+                   (Math.Abs(objA.Width - objB.Width) < threshold) &&
+                   (Math.Abs(objA.Height - objB.Height) < threshold);
+        }
+
+    }
+}
