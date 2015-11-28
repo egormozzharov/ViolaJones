23e4782cebd6904ec9667a137ca8a71d1001a4e8
MEgor <eguar@tut.by>
2015-11-28 18:56:03 +0300

    Nose detection added

--- Sources/FaceDetection/MainForm.cs
+++ Sources/FaceDetection/MainForm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 using Emgu.CV;
 using Emgu.CV.Structure;
@@ -14,11 +15,13 @@ namespace FaceDetection
 		private const string faceHaarCascadePath = @"Cascades\haarcascade_frontalface_alt.xml";
 		private const string eyeHaarCascadePath = @"Cascades\haarcascade_eye_treeglasses.xml";
 		private const string mouthHaarCascadePath = @"Cascades\haarcascade_mouth.xml";
+		private const string noseHaarCascadePath = @"Cascades\haarcascade_nose.xml";
 
 		private string photoFileName;
 		private CascadeClassifier _faceCascadeClassifier;
 		private CascadeClassifier _eyeCascadeClassifier;
 		private CascadeClassifier _mouthCascadeClassifier;
+		private CascadeClassifier _noseCascadeClassifier;
 
 		private Capture _capture = null;
 
@@ -29,6 +32,7 @@ namespace FaceDetection
 			_faceCascadeClassifier = new CascadeClassifier(faceHaarCascadePath);
 			_eyeCascadeClassifier = new CascadeClassifier(eyeHaarCascadePath);
 			_mouthCascadeClassifier = new CascadeClassifier(mouthHaarCascadePath);
+			_noseCascadeClassifier = new CascadeClassifier(noseHaarCascadePath);
 		}
 
 		#region EventActions
@@ -64,28 +68,66 @@ namespace FaceDetection
 		private void Detect_buttonClick(object sender, EventArgs e)
 		{
 			Image<Bgr, Byte> imageFrame = new Image<Bgr, Byte>((Bitmap)this.fixedPicture.Image);
-			var grayframe = imageFrame.Convert<Gray, byte>();
+			Image<Gray, byte> frame = imageFrame.Convert<Gray, byte>();
 
-			IEnumerable<Rectangle> faces = _faceCascadeClassifier.DetectMultiScale(grayframe, 1.1, 1, Size.Empty);
-			foreach (Rectangle face in faces)
+			IEnumerable<Rectangle> faces = _faceCascadeClassifier.DetectMultiScale(frame, 1.1, 1, Size.Empty);
+			if (faces.Any())
 			{
+				Rectangle face = faces.First();
 				imageFrame.Draw(face, new Bgr(Color.BurlyWood));
-			}
 
-
-			IEnumerable<Rectangle> eyes = _eyeCascadeClassifier.DetectMultiScale(grayframe, 1.1, 2, Size.Empty);
-			foreach (Rectangle eye in eyes)
-			{
-				imageFrame.Draw(eye, new Bgr(Color.Aqua));
-			}
-
-			IEnumerable<Rectangle> mouths = _mouthCascadeClassifier.DetectMultiScale(grayframe, 1.1, 1, Size.Empty);
-			foreach (Rectangle mouth in mouths)
-			{
-				imageFrame.Draw(mouth, new Bgr(Color.Aqua));
+				EyesDetection(imageFrame, frame, face);
+				MouthDetection(imageFrame, frame, face);
+				NoseDetection(imageFrame, frame, face);
 			}
 
 			fixedPicture.Image = imageFrame.Bitmap;
+		}
+
+		private void EyesDetection(Image<Bgr, Byte> imageFrame, Image<Gray, byte> frame, Rectangle face)
+		{
+			Rectangle eyesFrame = new Rectangle(face.X, face.Y, face.Width, face.Height / 2);
+			imageFrame.Draw(eyesFrame, new Bgr(Color.Aqua), 4);
+
+			Image<Gray, byte> eyesRegion = frame.Copy(eyesFrame);
+			IEnumerable<Rectangle> eyes = _eyeCascadeClassifier.DetectMultiScale(eyesRegion, 1.1, 2, Size.Empty);
+			foreach (Rectangle eye in eyes)
+			{
+				Rectangle eyeAbsolute = new Rectangle(eye.X + face.X, eye.Y + face.Y, eye.Width, eye.Height);
+				imageFrame.Draw(eyeAbsolute, new Bgr(Color.Aqua));
+			}
+		}
+
+		private void MouthDetection(Image<Bgr, Byte> imageFrame, Image<Gray, byte> frame, Rectangle face)
+		{
+			int mouthFrameHeight = Convert.ToInt32(face.Height / 4);
+			int mouthFrameY = face.Y + Convert.ToInt32(face.Height * 0.75);
+			Rectangle mouthFrame = new Rectangle(face.X, mouthFrameY, face.Width, mouthFrameHeight);
+			imageFrame.Draw(mouthFrame, new Bgr(Color.Red), 4);
+
+			Image<Gray, byte> mouthRegion = frame.Copy(mouthFrame);
+			IEnumerable<Rectangle> mouths = _mouthCascadeClassifier.DetectMultiScale(mouthRegion, 1.1, 1, Size.Empty);
+			foreach (Rectangle mouth in mouths)
+			{
+				Rectangle mouthAbsolute = new Rectangle(mouth.X + face.X, mouth.Y + mouthFrameY, mouth.Width, mouth.Height);
+				imageFrame.Draw(mouthAbsolute, new Bgr(Color.Red));
+			}
+		}
+
+		private void NoseDetection(Image<Bgr, Byte> imageFrame, Image<Gray, byte> frame, Rectangle face)
+		{
+			int noseFrameHeight = Convert.ToInt32(face.Height * 0.25); 
+			int noseFrameY = face.Y + Convert.ToInt32(face.Height * 0.5);
+			Rectangle noseFrame = new Rectangle(face.X, noseFrameY, face.Width, noseFrameHeight);
+			imageFrame.Draw(noseFrame, new Bgr(Color.BurlyWood), 4);
+
+			Image<Gray, byte> noseRegion = frame.Copy(noseFrame);
+			IEnumerable<Rectangle> noses = _noseCascadeClassifier.DetectMultiScale(noseRegion, 1.1, 2, Size.Empty);
+			foreach (Rectangle nose in noses)
+			{
+				Rectangle eyeAbsolute = new Rectangle(nose.X + face.X, nose.Y + noseFrameY, nose.Width, nose.Height);
+				imageFrame.Draw(eyeAbsolute, new Bgr(Color.BurlyWood));
+			}
 		}
 
 		#endregion
