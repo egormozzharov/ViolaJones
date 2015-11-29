46b193f3a964613497b8ef659d09ffba96290f09
MEgor <eguar@tut.by>
2015-11-29 13:02:12 +0300

    Created markers for centers and coordinates

--- Sources/FaceDetection/MainForm.Designer.cs
+++ Sources/FaceDetection/MainForm.Designer.cs
@@ -35,7 +35,6 @@
 			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
 			this.panel2 = new System.Windows.Forms.Panel();
 			this.catchPicture_button = new System.Windows.Forms.Button();
-			this.startButton = new System.Windows.Forms.Button();
 			this.webCamPicture = new System.Windows.Forms.PictureBox();
 			((System.ComponentModel.ISupportInitialize)(this.fixedPicture)).BeginInit();
 			this.statusStrip1.SuspendLayout();
@@ -58,9 +57,9 @@
 			// button1
 			// 
 			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-			this.button1.Location = new System.Drawing.Point(791, 18);
+			this.button1.Location = new System.Drawing.Point(469, 18);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(109, 46);
+			this.button1.Size = new System.Drawing.Size(431, 46);
 			this.button1.TabIndex = 5;
 			this.button1.Text = "Detect";
 			this.button1.UseVisualStyleBackColor = true;
@@ -96,7 +95,6 @@
 			// panel2
 			// 
 			this.panel2.Controls.Add(this.catchPicture_button);
-			this.panel2.Controls.Add(this.startButton);
 			this.panel2.Controls.Add(this.button1);
 			this.panel2.Location = new System.Drawing.Point(12, 428);
 			this.panel2.Name = "panel2";
@@ -105,23 +103,13 @@
 			// 
 			// catchPicture_button
 			// 
-			this.catchPicture_button.Location = new System.Drawing.Point(654, 18);
+			this.catchPicture_button.Location = new System.Drawing.Point(16, 19);
 			this.catchPicture_button.Name = "catchPicture_button";
-			this.catchPicture_button.Size = new System.Drawing.Size(106, 45);
+			this.catchPicture_button.Size = new System.Drawing.Size(431, 45);
 			this.catchPicture_button.TabIndex = 1;
 			this.catchPicture_button.Text = "Catch";
 			this.catchPicture_button.UseVisualStyleBackColor = true;
 			this.catchPicture_button.Click += new System.EventHandler(this.CatchPicture_button_Click);
-			// 
-			// startButton
-			// 
-			this.startButton.Location = new System.Drawing.Point(12, 18);
-			this.startButton.Name = "startButton";
-			this.startButton.Size = new System.Drawing.Size(114, 46);
-			this.startButton.TabIndex = 0;
-			this.startButton.Text = "Start";
-			this.startButton.UseVisualStyleBackColor = true;
-			this.startButton.Click += new System.EventHandler(this.StartButton_Click);
 			// 
 			// webCamPicture
 			// 
@@ -161,7 +149,6 @@
         private System.Windows.Forms.StatusStrip statusStrip1;
 		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
 		private System.Windows.Forms.Panel panel2;
-		private System.Windows.Forms.Button startButton;
 		private System.Windows.Forms.PictureBox webCamPicture;
 		private System.Windows.Forms.Button catchPicture_button;
     }
